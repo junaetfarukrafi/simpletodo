986a9efe728de790ba902c926f135d2046de6860
junaet faruk rafi <rafee790@gmail.com>
2022-11-04 21:30:35 +0600

    CRUD

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="heading1">
+      <h1>Home Page</h1>
+    <p className="homeclass">Please Go to the "Add Book" Option and add a book then check "Show Books" option then you will find all books which is added on list.And from "Show Books" page you can Edit & Delete any book.Basically its a CRUD APP.
+    <br/>
+     <br />
+      For this application i use technology:
+      <br />
+      #HTML(JSX),CSS
+      <br />
+      #REDUX
+      <br />
+      #REACT JS (At this moment I am not familiar with next js that's why I use reactjs. I hope you consider.)
+    </p>
+    </div>
+  );
+};
+
+export default Home;
